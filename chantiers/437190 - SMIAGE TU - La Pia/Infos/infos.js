7783748b3d4d266547f1b5fdce0ca8e04afb4292
Angelo-topo <a.losciale@razel-bec.fayat.com>
2025-06-05 22:22:06 +0200

    Add files via upload

---
+++ chantiers/437190 - SMIAGE TU - La Pia/Infos/infos.js
@@ -0,0 +1,18 @@
+const infosMarche = {
+  "maitreOuvrage": "SMIAGE",
+  "maitreOeuvre": "Razel-Bec, Garelli",
+  "objetMarche": "Travaux d'aménagement",
+  "site": " Barun Tende, Provence-Alpes-Côte d'Azur",
+  "conducteurTravaux": {
+    "fonction": "Conducteur de travaux",
+    "nom": "Didier FASSI",
+    "tel": "06 12 83 84 51"
+  },
+  "chefChantier": {
+    "fonction": "Chef de chantier",
+    "nom": "David FOUR",
+    "tel": "0"
+  },
+  "numAffaire": "437190",
+  "annee": "2024"
+};
