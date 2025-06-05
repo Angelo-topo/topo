97960c10ef55a557431d4c97d8977e0192083ae9
Angelo-topo <a.losciale@razel-bec.fayat.com>
2025-06-05 22:19:07 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+app.use(express.static('chantiers'));
 
 app.get('/list-chantier', (req, res) => {
-  const dossier = path.join(__dirname, 'public', 'chantier');
+  const dossier = path.join(__dirname, 'chantiers');
   fs.readdir(dossier, (err, fichiers) => {
     if (err) return res.status(500).json({ error: 'Erreur' });
     res.json(fichiers.filter(f => fs.statSync(path.join(dossier, f)).isFile()));
